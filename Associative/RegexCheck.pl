cb7975e966c915337cb4f1e9dfaf781b9a8d849f
Robyn Berkel <rberkel@purdue.edu>
2016-08-12 19:49:49 -0400

    scanner regex

--- Associative/RegexCheck.pl
+++ Associative/RegexCheck.pl
@@ -14,7 +14,7 @@ print "1:true\n" if $program =~ /public[[:space:]]*class[[:space:]]*Associative/
 print "2:true\n" if $program =~ /static[[:space:]]*void[[:space:]]*main[[:space:]]*\([[:space:]]*String[[:space:]]*(\[\])*[[:space:]]*[[:alnum:]]*[[:space:]]*(\[\])*[[:space:]]*\)/s;
 print "3:true\n" if $program =~ /(int|double)[[:space:]]*firstTwo[[:space:]]*\([[:space:]]*\)/s;
 print "4:true\n" if $program =~ /(int|double)[[:space:]]*lastTwo[[:space:]]*\([[:space:]]*\)/s;
-print "5:true\n" if $program =~ /main.*{.*Scanner[[:space:]]*[[:alnum:]]*[[:space:]]=[[:space:]]new[[:space:]]Scanner[[:space:]]*\([[:space:]]System.in[[:space:]]\)[[:space:]];/s;
+print "5:true\n" if $program =~ /main.*{.*Scanner[[:space:]]*[[:alnum:]]*[[:space:]]*=[[:space:]]*new[[:space:]]*Scanner[[:space:]]*\([[:space:]]*System\.in[[:space:]]*\);/s;
 print "6:true\n" if (($program =~ /[[:alnum:]]\.mySchool\(.*\)/s) && 
 	($program =~ /[[:alnum:]]\.myAge\(.*\)/s) && 
 	($program =~ /[[:alnum:]]\.myName\(.*\)/s));
