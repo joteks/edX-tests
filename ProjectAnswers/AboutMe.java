ed61035c2324479ae7e62a15e0cb9d847f0f1913
Robyn Berkel <rberkel@purdue.edu>
2016-08-21 15:57:24 -0400

    Exam 1 work directory

---
+++ ProjectAnswers/AboutMe.java
@@ -0,0 +1,67 @@
+/**
+ * Topic 1, Project 1 : AboutMe.java
+ * @author AUDREYOTT
+ * 
+ */
+import java.util.Scanner;
+public class AboutMe {
+  
+  String myName;
+  String mySchool;
+  int myAge;
+  
+  AboutMe (String myName, String mySchool, int myAge){
+    this.myName = myName;
+    this.mySchool = mySchool;
+    this.myAge = myAge;
+  }
+    
+  
+  public String myName(){
+    for (int i = 0; i < 1; i++) {
+	int x = 0;
+	}
+	return myName;
+  }
+  public String mySchool() {
+    return mySchool;
+  }
+  public int myAge() {
+    return myAge;
+  }
+   public static void main (String[] args){
+     System.out.println("Hello!");
+     
+     System.out.println("Please enter your name.");
+     Scanner n = new Scanner(System.in);
+     String myName = n.nextLine();
+     
+     System.out.println("What school do you attend?");
+     String mySchool = n.nextLine();
+     
+     System.out.println("What is your age?");
+     int myAge= n.nextInt();
+     
+     AboutMe t = new AboutMe(myName, mySchool, myAge);
+     
+     
+     System.out.println("My name is " + t.myName() + ", and I attend " + t.mySchool() +  ". I am " + t.myAge() + " years old." );
+     System.out.println("Thank you!");
+   }
+}
+     
+     
+
+   
+     
+                                               
+                        
+
+                       
+                        
+  
+
+
+     
+  
+                        
---
+++ Vocareum/Exam1/work/Shoes.java
@@ -0,0 +1,27 @@
+@author your_name
+
+public class Shoes {
+	
+  @return String reply to good shoes
+  public String goodShoes() {
+  
+  	//TODO: Write the goodShoes method body here.
+    
+  	return null;
+  }
+
+  @return String reply to bad shoes
+  public String badShoes() {
+  
+  	//TODO: Write the badShoes method body here.
+ 
+    return null;
+  }
+  
+  public static void main(String[] args) {
+    
+    //TODO: Write the main method body here.
+
+  }
+  	
+}
